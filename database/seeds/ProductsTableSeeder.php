154dca2e0532f0c7443aed5c369cb793b008dd51
AntonioTorralba96 <39259982+AntonioTorralba96@users.noreply.github.com>
2019-11-04 16:15:19 +0100

    Edición de los precios

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,7 +14,7 @@ class ProductsTableSeeder extends Seeder
     {
       DB::table('products')->insert([
             'name' => 'Tarta',
-            'price' => '3,90€',
+            'price' => '3,90',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo purus sit amet dui pharetra, nec ornare justo accumsan. Nam pharetra elit vitae venenatis pretium. Etiam eu massa ut ligula egestas volutpat vitae non mi. Vestibulum ut elit eu nunc pharetra ultrices nec sed enim. Pellentesque tellus orci, dapibus ac.',
             'shop_id' => '1',
             'poster' => 'https://canalcocina.es/medias/_cache/zoom-a5352c6b3b7e0845a2476ef5d6088a1e-920-518.jpg'
@@ -22,7 +22,7 @@ class ProductsTableSeeder extends Seeder
 
       DB::table('products')->insert([
             'name' => 'Vestido Azul',
-            'price' => '5€',
+            'price' => '5',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo purus sit amet dui pharetra, nec ornare justo accumsan. Nam pharetra elit vitae venenatis pretium. Etiam eu massa ut ligula egestas volutpat vitae non mi. Vestibulum ut elit eu nunc pharetra ultrices nec sed enim. Pellentesque tellus orci, dapibus ac.',
             'shop_id' => '2',
             'poster' => 'https://mlmirror.com/220-large_default/vestido-azul-largo.jpg'
@@ -30,7 +30,7 @@ class ProductsTableSeeder extends Seeder
 
       DB::table('products')->insert([
             'name' => 'Bombilla',
-            'price' => '1,50€',
+            'price' => '1,50',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo purus sit amet dui pharetra, nec ornare justo accumsan. Nam pharetra elit vitae venenatis pretium. Etiam eu massa ut ligula egestas volutpat vitae non mi. Vestibulum ut elit eu nunc pharetra ultrices nec sed enim. Pellentesque tellus orci, dapibus ac.',
             'shop_id' => '3',
             'poster' => 'https://www.creative-cables.co/47053-thickbox_default/bombilla-dorada-led-globo-g125-filamento-largo-6w-decorativa-vintage-2200k.jpg'
@@ -38,7 +38,7 @@ class ProductsTableSeeder extends Seeder
 
       DB::table('products')->insert([
             'name' => 'Pechuga de Pollo',
-            'price' => '5€',
+            'price' => '5',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo purus sit amet dui pharetra, nec ornare justo accumsan. Nam pharetra elit vitae venenatis pretium. Etiam eu massa ut ligula egestas volutpat vitae non mi. Vestibulum ut elit eu nunc pharetra ultrices nec sed enim. Pellentesque tellus orci, dapibus ac.',
             'shop_id' => '4',
             'poster' => 'https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/03/13/img_cperezg_20180313-130600_imagenes_lv_terceros_istock-865342632-kDnD-U441494954925yuE-992x558@LaVanguardia-Web.jpg'
