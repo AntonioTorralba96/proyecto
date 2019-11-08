c744f884609b077b2f907ffbe3f6a9f016df2592
AntonioTorralba96 <39259982+AntonioTorralba96@users.noreply.github.com>
2019-11-08 18:29:26 +0100

    creación de la tabla carrito

---
+++ database/seeds/ListsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ListsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('lists')->insert([
+            'user_id' => '1',
+            'product_id' => '1'
+        ]);
+
+        DB::table('lists')->insert([
+            'user_id' => '3',
+            'product_id' => '2'
+        ]);
+
+        DB::table('lists')->insert([
+            'user_id' => '2',
+            'product_id' => '4'
+        ]);
+
+        DB::table('lists')->insert([
+            'user_id' => '1',
+            'product_id' => '2'
+        ]);
+    }
+}
